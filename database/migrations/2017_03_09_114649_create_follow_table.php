fc323e1d0900e9d9225decfbb45fe094a3c8cef6
DiegoWaz <waziri.diego@gmail.com>
2017-03-09 18:01:22 +0000

    Modification de la database

--- database/migrations/2017_03_09_114649_create_follow_table.php
+++ database/migrations/2017_03_09_114649_create_follow_table.php
@@ -27,6 +27,6 @@ class CreateFollowTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('follow');
     }
 }
--- database/migrations/2017_03_09_130101_create_like_table.php
+++ database/migrations/2017_03_09_130101_create_like_table.php
@@ -28,6 +28,6 @@ class CreateLikeTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('like');
     }
 }
